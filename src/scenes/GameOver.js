43c5f1953ff251d8318a3e59d69fc8af4f5e41db
memofrezzo <memofrezzo@gmail.com>
2023-10-03 17:35:13 -0300

    créditos agregados y game over mejorado

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -6,19 +6,63 @@ export default class GameOver extends Phaser.Scene {
   }
 
   create() {
-    // Configura el mensaje de Game Over en el centro de la pantalla
     const centerX = this.cameras.main.width / 2;
     const centerY = this.cameras.main.height / 2;
-    const mensajeGameOver = this.add.text(centerX, centerY, "Game Over", {
+
+    // Mensaje de Game Over
+    const mensajeGameOver = this.add.text(centerX, centerY - 50, "Game Over", {
       fontFamily: 'Arial',
       fontSize: 48,
       color: '#ff0000', // Color rojo
     });
     mensajeGameOver.setOrigin(0.5);
 
-    // Habilita la interacción del usuario haciendo clic en cualquier parte de la pantalla
-    this.input.on('pointerdown', () => {
-      this.scene.start('Menu'); // Cambia a la escena del menú principal
+    // Botón de Restart
+    const botonRestart = this.add.text(centerX, centerY + 50, "Restart", {
+      fontFamily: 'Arial',
+      fontSize: 24,
+      color: '#ffffff', // Color blanco
+    });
+    botonRestart.setOrigin(0.5);
+
+    // Agranda el texto cuando el mouse se acerca al botón
+    botonRestart.on('pointerover', () => {
+      botonRestart.setScale(1.2); // Escala el texto al 120%
+    });
+
+    // Achica el texto cuando el mouse se aleja del botón
+    botonRestart.on('pointerout', () => {
+      botonRestart.setScale(1); // Restaura la escala original
+    });
+
+    botonRestart.setInteractive(); // Habilita la interacción con el botón
+    botonRestart.on('pointerdown', () => {
+      // Cuando se hace clic en el botón Restart, reinicia el nivel1
+      this.scene.start('Nivel1');
+    });
+
+    // Botón de Volver al Menú Principal
+    const botonVolverMenu = this.add.text(centerX, centerY + 100, "Volver al Menú Principal", {
+      fontFamily: 'Arial',
+      fontSize: 24,
+      color: '#ffffff', // Color blanco
+    });
+    botonVolverMenu.setOrigin(0.5);
+
+    // Agranda el texto cuando el mouse se acerca al botón
+    botonVolverMenu.on('pointerover', () => {
+      botonVolverMenu.setScale(1.2); // Escala el texto al 120%
+    });
+
+    // Achica el texto cuando el mouse se aleja del botón
+    botonVolverMenu.on('pointerout', () => {
+      botonVolverMenu.setScale(1); // Restaura la escala original
+    });
+
+    botonVolverMenu.setInteractive(); // Habilita la interacción con el botón
+    botonVolverMenu.on('pointerdown', () => {
+      // Cuando se hace clic en el botón de volver al menú principal, cambia a la escena del menú
+      this.scene.start('Menu');
     });
   }
 }
--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -4,7 +4,6 @@ export default class Preload extends Phaser.Scene {
   // escena para optimiozar tiempos
   // carga el preload solo una vez y sirve para todo el juego
   constructor() {
-    // key of the scene
     super("Preload");
   }
 
